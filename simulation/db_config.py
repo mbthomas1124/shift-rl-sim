7f95a499d4cc94b4b5734ca9e7e4a10988a8714c
mbthomas1124 <mthomas3@stevens.edu>
2023-06-23 16:33:08 -0400

    modified code to assume cfg files are in root directory

--- simulation/db_config.py
+++
@@ -1,5 +0,0 @@
-HOST = "10.246.103.16"
-USER = "hanlonpgsql4"
-PASSWORD = "XIfyqPqM446M"
-DATABASE = "shift_brokeragecenter"
-db_string = f'postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}/{DATABASE}'
