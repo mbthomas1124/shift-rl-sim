f6df9657d357f9b8a8707b7508fe479f5dc4c675
mbthomas1124 <mthomas3@stevens.edu>
2023-06-28 12:45:35 -0400

    bug fixes with env setup

--- env_setup/test_env.py
+++ env_setup/test_env.py
@@ -10,8 +10,10 @@ import tianshou
 import time
 import shift
 
-assert shift.__version__ == "2.0.1"
-assert torch.__version__ == "1.13.1"
-assert gym.__version__ == "0.26.2"
-assert gymnasium.__version__ == "0.28.1"
-assert tianshou.__version__ == "0.5.1"
+assert shift.__version__[:5] == "2.0.1"
+assert torch.__version__[:6] == "1.13.1"
+assert gym.__version__[:6] == "0.26.2"
+assert gymnasium.__version__[:6] == "0.28.1"
+assert tianshou.__version__[:5] == "0.5.1"
+
+print("ENVIRONMENT VERIFIED")
